8f5aa3c66b75f5bdae1028c18330336e14f27633
pri0818 <priyanshusinghal0818@gmail.com>
2023-01-12 10:40:25 +0000

    RMX2061: Merge RMX2061 device tree into common tree
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

---
+++ lineage_RMX2061.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2020 The DotOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from RMX2061 device
+$(call inherit-product, device/realme/RMX2061/device.mk)
+
+# Hotword
+$(call inherit-product, vendor/realme/hotword/google.mk)
+
+# DerpFest Flags
+SUSHI_BOOTANIMATION := 1080
+WITH_GMS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_ENABLE_PIXEL_GBOARD_PADDINGS := false
+
+# Inherit some common stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier
+PRODUCT_NAME := lineage_RMX2061
+PRODUCT_DEVICE := RMX2061
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX2061
+PRODUCT_MANUFACTURER := realme
+
+PRODUCT_SYSTEM_NAME := RMX2061
+PRODUCT_SYSTEM_DEVICE := RMX2061
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+BUILD_FINGERPRINT := "realme/RMX2061/RMX2061L1:11/RKQ1.201112.002/1649930822295:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2061 \
+    TARGET_PRODUCT=RMX2061 \
+    PRIVATE_BUILD_DESC="RMX2061-user 11 RKQ1.201112.002 1649930822295 release-keys"
