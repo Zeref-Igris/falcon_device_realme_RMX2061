5d36b60d1505476f8557f7e2138ec9728f87a0ec
Dyneteve <sound0020@gmail.com>
2023-04-28 08:40:34 +0000

    RMX2061: Enable AOSP surfaceflinger
    * AOSP SurfaceFlinger is smoother than CAF
    
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,6 +219,9 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Treble
 BOARD_VNDK_VERSION := current
 
